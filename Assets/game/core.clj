d9d52e818ae0b48657267a07219df227e1fa2b53
Artur Dębkowski <arturdebkowski@gmail.com>
2017-05-02 15:16:40 +0200

    current file

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -2,3 +2,22 @@
   (use arcadia.core))
 
 (log "asdf")
+
+(def ^:dynamic bingo-board (partition 15 (range 1 76)))
+
+(defn rand-part [coll]
+  (def ^:dynamic part coll)
+  (let [r (rand-nth part)]
+    (set! part (remove #(= % r)))
+    r))
+
+(rand-part (first bingo-board))
+
+(doseq [part bingo-board]
+  (dotimes [i 5]
+    (let [r (rand-nth part)]
+      (println r)
+      (set! part (remove #(= % r) part))))
+  (println part))
+
+(remove #(= % 1) bingo-board)
