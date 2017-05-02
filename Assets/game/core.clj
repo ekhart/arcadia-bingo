1d2a46421fd0dbf6c751749675673a3bf5397474
Artur Dębkowski <arturdebkowski@gmail.com>
2017-05-02 16:30:35 +0200

    working rand five

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -11,6 +11,36 @@
     (set! part (remove #(= % r)))
     r))
 
+;; (defn a [l part]
+;;   (recur (conj 
+
+(defn add [coll itm]
+  (let [r (rand-nth (range 1 16))]
+    (if (< (count coll) 5)
+      (conj coll (rand-nth (range 1 16))) 
+      coll)))
+
+;; working
+(reduce add #{} (range 1 16))
+
+(defn add [part]
+  (fn [coll itm]
+    (let [r (rand-nth part)]
+      (if (< (count coll) 5)
+        (conj coll r)) 
+        coll)))
+
+(conj #{} 1)
+
+(some #(= 1 %) [1 2 3])
+
+(defn rand-5 [coll]
+  (reduce (add coll) #{} coll))
+
+((add (range 16 31)) #{} 1)
+
+(rand-5 (range 16 31))
+
 (rand-part (first bingo-board))
 
 (doseq [part bingo-board]
